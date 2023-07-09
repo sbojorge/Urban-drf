aaa6c06777e643aa3f4e0af279473f11ca366dc2
sbojorge <sara.bojorge@gmail.com>
2023-07-09 15:08:45 +0000

    Install Cloudinary, set its env vars and configuration

--- urban_drf/settings.py
+++ urban_drf/settings.py
@@ -16,6 +16,13 @@ import dj_database_url
 if os.path.exists('env.py'):
     import env
 
+CLOUDINARY_STORAGE = {
+    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
+}
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,6 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary-storage', # as per the documentation, in this order because cloudinary will be used only for media files
+    'cloudinary',
 
     # Apps
     'profiles',

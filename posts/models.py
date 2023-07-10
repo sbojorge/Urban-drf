63be73b53168e4ded8bfc3032c829e5cced16e5b
sbojorge <sara.bojorge@gmail.com>
2023-07-10 11:39:43 +0000

    Fix the video upload to Cloudinary

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary_storage.storage import VideoMediaCloudinaryStorage
-from cloudinary_storage.validators import validate_video
+from django.core.validators import FileExtensionValidator
 
+ext_validator = FileExtensionValidator(['mp4', 'webm', 'flv', 'mov', 'ogv', '3gp', '3g2', 'wmv',
+                                        'mpeg', 'mkv', 'avi'])
 
 class Post(models.Model):
     """
@@ -13,8 +15,9 @@ class Post(models.Model):
     image = models.ImageField(
         upload_to='images/', default='../old-time-camera.512x422_iwlbmx', blank=True
     )
-    video = models.ImageField(
-        upload_to='videos/', default='../film-camera.512x512_qa6tan', blank=True
+    video = models.FileField(
+        upload_to='videos/', default='../film-camera.512x512_qa6tan', blank=True,
+        storage=VideoMediaCloudinaryStorage(), validators=[ext_validator]
     )
     content = models.TextField(blank=True)
     created_on=models.DateTimeField(auto_now_add=True)

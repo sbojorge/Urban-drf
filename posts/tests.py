accf10f1ccbd88144ffbe0af0a74174357e70f67
sbojorge <sara.bojorge@gmail.com>
2023-07-11 22:55:40 +0000

    Run tests for retrieving and updating a post by id

--- posts/tests.py
+++ posts/tests.py
@@ -16,19 +16,46 @@ class PostListCreateViewTests(APITestCase):
         response = client.get('/posts/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    # def test_authenticated_user_can_create_post(self):
-    #     client = APIClient()
-    #     client.login(username='ella', password='rabbit')
-    #     response = client.post('/posts/', {'title': 'a city trip'})
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    def test_authenticated_user_can_create_post(self):
+        client = APIClient()
+        client.login(username='ella', password='rabbit')
+        response = client.post('/posts/', {'title': 'a city trip'})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
     
-    # def test_unauthenticated_user_cant_create_post(self):
-    #     client = APIClient()
-    #     response = client.post('/posts/', {'title': 'a city trip'})
-    #     self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    def test_unauthenticated_user_cant_create_post(self):
+        client = APIClient()
+        response = client.post('/posts/', {'title': 'a city trip'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
+class PostRetrieveUpdateDestroyViewTests(APITestCase):
+    def setUp(self):
+        ella = User.objects.create_user(username='ella', password='rabbit')
+        damien = User.objects.create_user(username='damien', password='cat')
+        Post.objects.create(
+            owner=ella, title='a fun city trip', content='only playgrounds'
+        )
+        Post.objects.create(
+            owner=damien, title='a food city trip', content='only restaurants'
+        )
     
-    
+    def test_can_retrieve_post_by_valid_id(self):
+        client = APIClient()
+        response = client.get('/posts/1')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_cant_retrieve_post_by_invalid_id(self):
+        client = APIClient()
+        response = client.get('/posts/15')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_owned_post(self):
+        client = APIClient()
+        client.login(username='ella', password='rabbit')
+        response = client.put('/posts/1', {'title': 'a game trip'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
     
-        
+    def test_user_cant_update_someoneelses_post(self):
+        client = APIClient()
+        client.login(username='ella', password='rabbit')
+        response = client.put('/posts/2', {'title': 'a sport trip'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
--- urban_drf/settings.py
+++ urban_drf/settings.py
@@ -127,13 +127,12 @@ if 'DEV' in os.environ:
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }   
-    }    
+    }
 else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-
-
+    
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 

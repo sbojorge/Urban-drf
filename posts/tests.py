f4c63269bd892fd64ea69e14c3872c78bd3decbe
sbojorge <sara.bojorge@gmail.com>
2023-07-11 14:53:46 +0000

    Run tests for retrieve list of posts and create a post

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+class PostListCreateViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='ella', password='rabbit')
+    
+    # def test_can_list_posts(self):
+    #     ella = User.objects.get(username='ella')
+    #     Post.objects.create(owner=ella, title='a city trip')
+    #     client = APIClient()
+    #     response = client.get('/posts/')
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_authenticated_user_can_create_post(self):
+        client = APIClient()
+        client.login(username='ella', password='rabbit')
+        response = client.post('/posts/', {'title': 'a city trip'})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_unauthenticated_user_cant_create_post(self):
+        client = APIClient()
+        response = client.post('/posts/', {'title': 'a city trip'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    
+    
+
+    
+        
--- urban_drf/settings.py
+++ urban_drf/settings.py
@@ -16,8 +16,8 @@ import dj_database_url
 if os.path.exists('env.py'):
     import env
 
-from django.conf import settings
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# from django.conf import settings
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL'),
@@ -28,7 +28,7 @@ MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,19 +121,17 @@ WSGI_APPLICATION = 'urban_drf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }   
+    }    
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 
 # Password validation

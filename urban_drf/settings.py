08fa367362c96e256d15adfb4ae59c6a55a7b458
sbojorge <sara.bojorge@gmail.com>
2023-07-19 23:14:38 +0000

    Fix typo in client origin in settings.py

--- urban_drf/settings.py
+++ urban_drf/settings.py
@@ -128,7 +128,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",

599aeef6daa39c208a5e0d4049dfc02cfd356a4b
sbojorge <sara.bojorge@gmail.com>
2023-07-21 11:00:49 +0000

    Set date-time format in settings.py

--- urban_drf/settings.py
+++ urban_drf/settings.py
@@ -35,6 +35,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y',
 }
 # Once in production it will send data only in json
 if 'DEV' not in os.environ:

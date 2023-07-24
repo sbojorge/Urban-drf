819766df10f49a6c9da989d4c29d03641755534b
sbojorge <sara.bojorge@gmail.com>
2023-07-24 05:42:45 +0000

    Run tests for the comments app

--- posts/tests.py
+++ posts/tests.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from .models import Post
 from rest_framework import status
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APITestCase
 
 
 class PostListCreateViewTests(APITestCase):

0d020e85d8154bd9b8605184a78d27f88f02ee98
sbojorge <sara.bojorge@gmail.com>
2023-07-27 20:47:40 +0000

    Add the following_id field in profiles

--- followers/views.py
+++ followers/views.py
@@ -1,12 +1,13 @@
-from rest_framework import generics, permissions
-from drf_api.permissions import IsOwnerOrReadOnly
+from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
+from rest_framework import permissions
+from urban_drf.permissions import IsOwnerOrReadOnly
 from .models import Follower
 from .serializers import FollowerSerializer
 
 
-class FollowerList(generics.ListCreateAPIView):
+class FollowerList(ListCreateAPIView):
     """
-    List all followers, i.e. all instances of a user
+    List all followers instances of a user
     following another user'.
     Create a follower, i.e. follow a user if logged in.
     Perform_create: associate the current logged in user with a follower.
@@ -19,7 +20,7 @@ class FollowerList(generics.ListCreateAPIView):
         serializer.save(owner=self.request.user)
 
 
-class FollowerDetail(generics.RetrieveDestroyAPIView):
+class FollowerDetail(RetrieveDestroyAPIView):
     """
     Retrieve a follower
     No Update view, as we either follow or unfollow users
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 from django_countries.serializers import CountryFieldMixin
 
 
@@ -10,6 +11,7 @@ class ProfileSerializer(CountryFieldMixin, serializers.ModelSerializer):
     owner = serializers.CharField(read_only=True, source='owner.username') # Overrides the default owner's behaviour
     is_owner = serializers.SerializerMethodField() # Add the is_owner field and its value to the serialized profile object
     posts_count = serializers.ReadOnlyField()
+    following_id = serializers.SerializerMethodField()
 
     class Meta:
         model = Profile
@@ -22,3 +24,14 @@ class ProfileSerializer(CountryFieldMixin, serializers.ModelSerializer):
             return True
         else:
             return False
+    
+
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            # print(following)
+            return following.id if following else None
+        return None

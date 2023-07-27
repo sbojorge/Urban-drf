c82b436e8878edd11fa6668c945141dbeef5db04
sbojorge <sara.bojorge@gmail.com>
2023-07-27 21:10:22 +0000

    Add the filterset_fields in the profiles views to get all the profiles followed by another profile

--- profiles/views.py
+++ profiles/views.py
@@ -21,6 +21,10 @@ class ProfileList(ListAPIView):
         filters.OrderingFilter,
         DjangoFilterBackend,
     ]
+    filterset_fields = [
+         'owner__following__followed__profile',
+         'owner__followed__owner__profile'
+    ]
     ordering_fields = [
         'posts_count',
         'followers_count',
